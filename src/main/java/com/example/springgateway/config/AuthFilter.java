d9ebc0572417c0c951fac405f2a9861be2cdb7ac
Willen Leiros <willenleiros@jfrn.jus.br>
2024-02-20 20:27:13 -0300

    feat: adicionando filtro para logs da requisição

--- src/main/java/com/example/springgateway/config/AuthFilter.java
+++ src/main/java/com/example/springgateway/config/AuthFilter.java
@@ -2,24 +2,37 @@ package com.example.springgateway.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@RefreshScope
 @Component
-public class AuthFilter implements GatewayFilter {
+public class AuthFilter implements GlobalFilter, Ordered {
 
     Logger logger = LoggerFactory.getLogger(AuthFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        logger.info(request.getURI().getHost()+request.getPath());
+        ServerHttpResponse response = exchange.getResponse();
+        logger.info("request: {}",String.format("Host: {%s}, Uri: {%s}, Content-Type: {%s}, Method: {%s}, " +
+                        "Response StatusCode: {%s}, Authorization: {%s}",
+                request.getHeaders().getHost(),
+                request.getURI(),
+                request.getHeaders().getContentType(),
+                request.getMethod().name(),
+                response.getStatusCode().value(),
+                request.getHeaders().get("Authorization")));
         return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
